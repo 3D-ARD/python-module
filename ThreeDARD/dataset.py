cb5c6fed6bc3af05fdcb0167803b9d49933f85b9
Nicolas Mellado <nmellado0@gmail.com>
2020-09-22 15:30:22 +0200

    Refactoring of the API
     - Rename internal functions as __functioname__
     - Update documentation, clarify notion of relativePath
     - Rename openFile as getSynchronizedFilePath
     - Add forceSync option to getAssetMeta, getUnitMeta and getSynchronizedFilePath

--- ThreeDARD/dataset.py
+++ ThreeDARD/dataset.py
@@ -4,11 +4,17 @@ import os
 import json
 
 __LOCAL_DATASET_PATH__ = None
+verbose = True
 
 class DatasetInitializationError(Exception):
     """Exception thrown when trying to access an uninitialized dataset"""
     pass
 
+################################################################################
+# Utilities
+################################################################################
+def verboseLog(message):
+    if verbose: print ("    " + message)
 
 def projectName():
     """Name of the project on archeogrid"""
@@ -20,7 +26,7 @@ def projectId():
 
 def init(localpath):
     """Initialize the dataset and link to the local filesystem
-    :args: Local path where the dataset is stored.
+    :args: Local path where the dataset is stored. Can be retrieved using getLocalPath()
     :raises: RuntimeError, OSError
     """
     global __LOCAL_DATASET_PATH__
@@ -34,7 +40,11 @@ def init(localpath):
 def isInitialized():
     return __LOCAL_DATASET_PATH__ is not None
 
-def checkInitialized():
+
+################################################################################
+# Internals
+################################################################################
+def __checkInitialized__():
     """Check if the dataset has been correctly initialized
     :seealso: init().
     :raises: DatasetInitializationError
@@ -42,43 +52,84 @@ def checkInitialized():
     if not isInitialized():
         raise DatasetInitializationError("Local path of the dataset has not been set.")
 
-def requestsFile(relativePath):
+def __fetchFile__(relativePath):
     """Use requests library to download file.
-    It is not recommended to call this function directly, prefer openFile
-    :seealso: openFile().
+    It is not recommended to call this function directly, prefer getSynchronizedFilePath
+    :seealso: getSynchronizedFilePath().
     """
     return requests.get("https://www-dev.archeogrid.fr/descaladen?type=E&idP=" + projectName() + "&path=" + relativePath, allow_redirects=True)
 
-def openFile(relativePath):
-    """Open a file of the dataset, and return its corresponding file object.
-    If the file is not in the filesystem, it is downloaded first.
-    :seealso: openFile, checkInitialized
+
+################################################################################
+# Getters
+################################################################################
+def getLocalPath():
+    __checkInitialized__()
+    return __LOCAL_DATASET_PATH__
+
+def getAssetRelativeDirectory(assetName):
+    """Relative path of a given asset.
+    Relative paths are expressed relatively to getLocalPath().
+    :seealso: init().
     """
-    checkInitialized()
+    return assetName
+
+def getUnitRelativeDirectory(assetName, unitName):
+    """Relative path of the unit of a given asset.
+    Relative paths are expressed relatively to getLocalPath().
+    :seealso: init().
+    """
+    return getAssetRelativeDirectory(assetName) + "/" + unitName
+
+
+################################################################################
+# Fetching and loading data
+################################################################################
+def getSynchronizedFilePath(relativePath, forceSync = False):
+    """Get the path on the current filesystem from a file in the dataset.
+    If the file is not in the filesystem, it is downloaded first.
+
+    This function is used to download and synchronise data from online dataset.
+    To access metadata, it is recommended to use getAssetMeta and getUnitMeta
+    directly.
+
+    :seealso: getAssetMeta, getUnitMeta
+    """
+    __checkInitialized__()
 
     global __LOCAL_DATASET_PATH__
-    localpath = __LOCAL_DATASET_PATH__ + "/" + relativePath
+    localpath = os.path.join(__LOCAL_DATASET_PATH__, relativePath)
 
     # check if file needs to be downloaded
-    if not os.path.exists(localpath):
+    if not os.path.exists(localpath) or forceSync:
         # download file
-        print ("    Fetching " + relativePath + " from online dataset...")
-        r = requestsFile(relativePath)
+        verboseLog ("Fetching " + relativePath + " from online dataset...")
+        r = __fetchFile__(relativePath)
 
         # recursively create local path if needed
         dirname   = os.path.dirname(localpath)
         if not os.path.exists(dirname):
-            print ("    Creating directory " + dirname)
+            verboseLog ("Creating directory " + dirname)
             os.makedirs(dirname)
 
         open(localpath, 'wb').write(r.content)
     else:
-        print("    Retrieve " + relativePath + " from cache")
+        verboseLog("Retrieve " + relativePath + " from cache")
     # return read only file descriptor
-    return open(localpath, 'r')
+    return localpath
 
-def getAssetMeta(assetName):
-    return json.load( openFile(assetName + "/asset.json") )
+def getAssetMeta(assetName, forceSync = False):
+    """Get the metadata of a given asset. The metadata file is fetched if needed.
+    :seealso: getSynchronizedFilePath()
+    """
+    relativePath = os.path.join(getAssetRelativeDirectory(assetName), "asset.json")
+    localpath = getSynchronizedFilePath( relativePath, forceSync )
+    return json.load( open(localpath, 'r') )
 
-def getUnitMeta(assetName, unitName):
-    return json.load( openFile(assetName + "/" + unitName + "/unit.json") )
+def getUnitMeta(assetName, unitName, forceSync = False):
+    """Get the metadata of a given unit. The metadata file is fetched if needed.
+    :seealso: getSynchronizedFilePath()
+    """
+    relativePath = os.path.join(getUnitRelativeDirectory(assetName, unitName), "unit.json")
+    localpath = getSynchronizedFilePath( relativePath, forceSync )
+    return json.load( open(localpath, 'r') )
