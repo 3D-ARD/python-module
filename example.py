7b45bb8bc57d05f5195306dc3c04de4a6d483607
Nicolas Mellado <nmellado0@gmail.com>
2020-09-22 15:31:32 +0200

    Illustrate new API by searching for .xyz files

--- example.py
+++ example.py
@@ -1,9 +1,9 @@
 from ThreeDARD import *
-import json
 
 assets = ["Crane_mesange"]
 
 dataset.init("./dataset")
+dataset.verbose = False
 
 for a in assets:
     print ("Processing asset " + a)
@@ -12,4 +12,16 @@ for a in assets:
     for u in asset["units"]:
         unit   = dataset.getUnitMeta(a, u)
         print ( "[" + u + "] " + unit["name"])
+
+        #check if any .xyz file can be found
+        for entry in unit["outputs"]:
+            if entry["filename"].endswith(".xyz"):
+
+                # 3d model found, getting relative path in the dataset
+                filename = dataset.getUnitRelativeDirectory(a, u) + "/" + entry["filename"]
+                print("3d model found in dataset: " + filename)
+
+                # download (if needed only) and get the 3d model
+                sfilename = dataset.getSynchronizedFilePath( filename)
+                print("3d model downloaded at " + sfilename)
 
