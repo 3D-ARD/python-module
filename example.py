517dd9c3493be119f47d1478f82e9c8d718e3bc4
Nicolas Mellado <nmellado0@gmail.com>
2020-09-22 16:18:07 +0200

    Handle json decode error, print errors

--- example.py
+++ example.py
@@ -1,27 +1,49 @@
 from ThreeDARD import *
+import colored, os
 
 assets = ["Crane_mesange"]
 
 dataset.init("./dataset")
 dataset.verbose = False
 
+def printError(message):
+    print(colored.stylize(message, colored.fg("red")))
+
+def printFile(filename):
+    print(colored.stylize(open(filename, 'r').read() , colored.bg("light_red")))
+
 for a in assets:
     print ("Processing asset " + a)
-    asset = dataset.getAssetMeta(a)
+    asset = None
+    try:
+        asset = dataset.getAssetMeta(a)
+    except (ValueError):
+        printError("Error when loading {} metadata".format(a))
+        #relativePath = os.path.join(dataset.getAssetRelativeDirectory(a), "asset.json")
+        #printFile(dataset.getSynchronizedFilePath( relativePath ))
+    else:
 
-    for u in asset["units"]:
-        unit   = dataset.getUnitMeta(a, u)
-        print ( "[" + u + "] " + unit["name"])
+        for u in asset["units"]:
+            unit = None
 
-        #check if any .xyz file can be found
-        for entry in unit["outputs"]:
-            if entry["filename"].endswith(".xyz"):
+            try:
+                unit = dataset.getUnitMeta(a, u)
+            except (ValueError):
+                printError ("Error when loading {} (unit {}) metadata".format(a, u))
+                #relativePath = os.path.join(dataset.getUnitRelativeDirectory(a,u), "unit.json")
+                #printFile(dataset.getSynchronizedFilePath( relativePath ))
+            else:
+                print ( "[" + u + "] " + unit["name"])
 
-                # 3d model found, getting relative path in the dataset
-                filename = dataset.getUnitRelativeDirectory(a, u) + "/" + entry["filename"]
-                print("3d model found in dataset: " + filename)
+                #check if any .xyz file can be found
+                for entry in unit["outputs"]:
+                    if entry["filename"].endswith(".xyz"):
 
-                # download (if needed only) and get the 3d model
-                sfilename = dataset.getSynchronizedFilePath( filename)
-                print("3d model downloaded at " + sfilename)
+                        # 3d model found, getting relative path in the dataset
+                        filename = dataset.getUnitRelativeDirectory(a, u) + "/" + entry["filename"]
+                        print("3d model found in dataset: " + filename)
+
+                        # download (if needed only) and get the 3d model
+                        sfilename = dataset.getSynchronizedFilePath( filename)
+                        print("3d model downloaded at " + sfilename)
 
