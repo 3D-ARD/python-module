39e8b5eb13bc673b2d7cfa2060cc410ede5435ab
Nicolas Mellado <nmellado0@gmail.com>
2020-09-23 15:42:39 +0200

    Explicitely check consistency between json outputs and files

--- example.py
+++ example.py
@@ -2,9 +2,9 @@ from ThreeDARD import *
 import colored, os
 
 dataset.init("./dataset")
-dataset.verbose = True
+dataset.verbose = False
 
-forceSync = True
+forceSync = False
 printFileContent = True
 fetch3D = False
 
@@ -17,15 +17,17 @@ def printFile(filename):
 
 d = dataset.getDatasetMeta(forceSync)
 for a in d["units"]:
-    print ("Processing asset " + a)
+    print ("\nProcessing asset " + a)
     asset = None
     try:
         asset = dataset.getAssetMeta(a, forceSync)
-    except (ValueError):
-        printError("Error when loading {} metadata".format(a))
+    except ValueError:
+        printError("Syntax error when parsing {} metadata".format(a))
         if printFileContent:
             relativePath = os.path.join(dataset.getAssetRelativeDirectory(a), "asset.json")
             printFile(dataset.getSynchronizedFilePath( relativePath ))
+    except dataset.RemoteFileNotFountError:
+        printError("FileNotFound: asset metadata not available on remote for asset {}".format(a))
     else:
 
         for u in asset["units"]:
@@ -34,12 +36,22 @@ for a in d["units"]:
             try:
                 unit = dataset.getUnitMeta(a, u, forceSync)
             except (ValueError):
-                printError ("Error when loading {} (unit {}) metadata".format(a, u))
+                printError ("[{}] Syntax error when parsing metadata".format(u))
                 if printFileContent:
                     relativePath = os.path.join(dataset.getUnitRelativeDirectory(a,u), "unit.json")
                     printFile(dataset.getSynchronizedFilePath( relativePath ))
+            except dataset.RemoteFileNotFountError:
+                printError("[{}] FileNotFound: unit metadata not available on remote)".format(u))
             else:
-                print ( "[" + u + "] " + unit["name"])
+#                print ( "[" + u + "] " + unit["name"] + " (software = " + str(unit["software"]) + "). Files: " + str(unit["outputs"]))
+                print ( "[" + u + "] " + unit["name"] + " (software = " + str(unit["software"]) + ").")
+
+
+                # check file consistency
+                for entry in unit["outputs"]:
+                    filename = dataset.getUnitRelativeDirectory(a, u) + "/" + entry["filename"]
+                    if not dataset.checkFileExistsOnRemote(filename):
+                        printError("[{}] FileNotFound: {}".format(u, filename))
 
                 if fetch3D:
                     #check if any .xyz file can be found
@@ -53,4 +65,3 @@ for a in d["units"]:
                             # download (if needed only) and get the 3d model
                             sfilename = dataset.getSynchronizedFilePath( filename)
                             print("3d model downloaded at " + sfilename)
-
