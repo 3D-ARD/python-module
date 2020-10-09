b383bb377dd301095d7c72e01acd642fd5b7ba3a
Nicolas Mellado <nmellado0@gmail.com>
2020-10-09 12:22:10 +0200

    improve messages output + check remaining fields

--- checkDatasetConsistency.py
+++ checkDatasetConsistency.py
@@ -17,7 +17,7 @@ import colored, os
 dataset.init("./dataset")
 dataset.verbose = False
 
-forceSync = True
+forceSync = False
 printFileContent = True
 
 def printError(message):
@@ -60,7 +60,12 @@ for a in d["assets"]:
             except dataset.RemoteFileNotFountError:
                 printError("[{}] FileNotFound: unit metadata not available on remote)".format(u))
             else:
-                print ( "[" + u + "] " + unit["name"] + " (software = " + str(unit["software"]) + ").")
+                print ("[{}] {} ".format(u, unit["name"]) + (colored.stylize("(process)", colored.fg("blue")) if unit["type"] == "process" else colored.stylize("(acquisition)", colored.fg("green"))))
+                print("   Author: " + unit["author"])
+                if unit["hardware"]: print ("   Hardware: " + str(unit["hardware"]) + ".")
+                if unit["software"]: print ("   Software: " + str(unit["software"]) + ".")
+                print("   Description: " + unit["description"])
+                print(("   Details: " + unit["details"] if not unit["details"] else "   No details given"))
 
 
                 # check file consistency
@@ -68,4 +73,33 @@ for a in d["assets"]:
                     filename = dataset.getUnitRelativeDirectory(a, u) + "/" + entry["filename"]
                     if not dataset.checkFileExistsOnRemote(filename):
                         printError("[{}] FileNotFound: {}".format(u, filename))
+
+                # check input relationships consistency
+                if unit["inputs"]:
+                    print( "   Takes as input {}".format(unit["inputs"]))
+                    for u2 in unit["inputs"]:
+                        try:
+                            unit = dataset.getUnitMeta(a, u2, forceSync)
+                        except dataset.RemoteFileNotFountError:
+                            printError("FileNotFound: unit {} takes as input unit {}, which does not exists in the dataset".format(u,u2))
+
+
+                # check input relationships consistency
+                if unit["related"]:
+                    print( "   Is related to {}".format(unit["inputs"]))
+                    for u2 in unit["related"]:
+                        try:
+                            unit = dataset.getUnitMeta(a, u2, forceSync)
+                        except dataset.RemoteFileNotFountError:
+                            printError("FileNotFound: unit {} is related to unit {}, which does not exists in the dataset".format(u,u2))
+
+
+
+
+
+
+
+
+
+
 
